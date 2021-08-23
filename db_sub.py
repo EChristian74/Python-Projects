c85af1e43441933270a2612c48e305aca63cba98
EChristian74 <75714720+EChristian74@users.noreply.github.com>
2021-08-23 13:48:29 -0500

    Data Submission Assignment v2

--- db_sub.py
+++ db_sub.py
@@ -2,61 +2,37 @@
 
 import sqlite3
 
-import glob, os
+
+# file list from which script will write to db
 
 
-# method to connect to a database
-
-"""
-    the below creates variable using class sqlite3 to 
-    connect to database using method of connect() 
-    also creates our empty database
-"""
 conn = sqlite3.connect('files.db')
 
-with conn:  # do this while we are using database
-    cur = conn.cursor()  # invokes the cursor object, then below is sql string statement passed in and database is opened
+with conn:  # do this while we are using db
+    cur = conn.cursor()  # invokes cursor object, then below sql statement passed in to open db
     cur.execute("CREATE TABLE IF NOT EXISTS tbl_file_names ( \
         ID INTEGER PRIMARY KEY AUTOINCREMENT, \
-        col_name STRING \
-        )")  # sql string statment
+        col_file_name STRING \
+        )")  # sql string statement
     conn.commit()  # commits the change
-conn.close() # closes the connection to database
+conn.close() # closes the connection to db
 
 
 
-conn = sqlite3.connect('files.db')  # re-connects to database
-                                 
-with conn:  # do this while we are using database
-    cur = conn.cursor()  # invokes the cursor object, then below is sql string statement passed in and database is opened
-    cur.execute("INSERT INTO tbl_file_names(col_name) VALUES(?)", \
-                ('information.docx',))  # string value sql statement
-    cur.execute("INSERT INTO tbl_file_names(col_name) VALUES(?)", \
-                ('Hello.txt',))  # string value sql statement
-    cur.execute("INSERT INTO tbl_file_names(col_name) VALUES(?)", \
-                ('myImage.png',))  # string value sql statement
-    cur.execute("INSERT INTO tbl_file_names(col_name) VALUES(?)", \
-                ('myMovie.mpg',))  # string value sql statement
-    cur.execute("INSERT INTO tbl_file_names(col_name) VALUES(?)", \
-                ('World.txt',))  # string value sql statement
-    cur.execute("INSERT INTO tbl_file_names(col_name) VALUES(?)", \
-                ('data.pdf',))  # string value sql statement
-    cur.execute("INSERT INTO tbl_file_names(col_name) VALUES(?)", \
-                ('myPhoto.jpg',))  # string value sql statement
-    conn.commit()  # commits the change
-conn.close()  # closes the connection to the database
-                
+conn = sqlite3.connect('files.db')  # re-connects to db
+
+# list from which for loop will gather data and write to db
+fileList = ('information.docx', 'Hello.txt', 'myImage.png', \
+            'myMovie.mpg', 'World.txt', 'data.pdf', 'myPhoto.jpg')
+
+# loops through each object in list above to gather file names that end in 'txt'
+for x in fileList:
+    if x.endswith('txt'):
+        with conn:
+            cur = conn.cursor()
+            # the value for each row in db will be one file name out of the tuple
+            # and will denote one element tuple for each file name per row ending with 'txt'
+            cur.execute("INSERT INTO tbl_file_names(col_file_name) VALUES (?)", (x,))
+            print(x) # final command to print to console data written to db
 
 
-conn = sqlite3.connect('files.db') # re-connects to database
-
-
-with conn:  # do this while we are using database
-    cur = conn.cursor()  # invokes the cursor object, then below is sql string statement passed in and database is opened
-    cur.execute("SELECT col_name FROM tbl_file_names WHERE col_name LIKE '%txt'") # tuple - fixed data
-    varName = cur.fetchall()  # stores data as variable, so isn't lost
-    for item in varName:
-        msg = "File Name: {}".format(item[0])  # calling data using wildcard
-    print(msg)  
-
-                                 
